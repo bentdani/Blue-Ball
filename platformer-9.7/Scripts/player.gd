7f1e5d1114154c144401cc56578f925ddfc5f9bf
EpsilonNano <epsilonnano@gmail.com>
2026-01-19 20:34:46 -0500

    fix

--- platformer-9.7/Scripts/player.gd
+++ platformer-9.7/Scripts/player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-var acceleration = 900.0
+var acceleration = 1800.0
 var max_speed = 150.0
 const JUMP_VELOCITY = -300.0
 
 var surface_frictions = {
-	"ice": 0.01,
+	"ice": 0.1,
 	"normal": 2.0
 }
 var friction = surface_frictions["normal"]
@@ -16,16 +16,23 @@ func check_if_on_ice() -> bool:
 		# Get the tile position under the player
 		var floor_pos = get_parent().get_node("Main").local_to_map(global_position + Vector2(0, 5))
 		var data = get_parent().get_node("Main").get_cell_tile_data(floor_pos)
-
+		
+		print("data: " + str(data))
+		print("floor pos " + str(floor_pos))
+		
 		if data:
 			return data.get_custom_data("is_ice")
+		else:
+			push_error("No block data for surface type!")
 	return false
 
 func _physics_process(delta: float) -> void:
 	if check_if_on_ice():
 		friction = surface_frictions["ice"]
+		print("ice!")
 	else:
 		friction = surface_frictions["normal"]
+		print("no ice!")
 	
 	# Read the gravity from the Stats
 	var gravity = Stats.gravity
